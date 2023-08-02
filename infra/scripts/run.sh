7b8102dbd381d9434b95ba7608bdcd7afb4703ab
Ryan LaLiberty <rycolos@gkserver.net>
2023-08-02 06:39:06 -0400

    run script updates

--- infra/scripts/run.sh
+++ infra/scripts/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mkdir -p /home/ubuntu/log
+touch -m /home/ubuntu/log/runlog.log
+echo date >> /home/ubuntu/log/runlog.log
+
 cd /home/ubuntu/fitness-tracker-pipeline
 git pull
 source dbt-venv/bin/activate

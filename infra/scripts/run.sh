0efce09c1061a1bc4ec27f1b32f026bbd1778d52
Ryan LaLiberty <rycolos@gkserver.net>
2023-08-02 06:20:57 -0400

    ec2 run script

--- infra/scripts/run.sh
+++ infra/scripts/run.sh
@@ -4,5 +4,5 @@ cd /home/ubuntu/fitness-tracker-pipeline
 git pull
 source dbt-venv/bin/activate
 cd dbt_fit
-dbt snapshot
-dbt run
+dbt snapshot --profiles-dir=profiles
+dbt run --profiles-dir=profiles

868ffa3d0ae90ec12971036f60a933b3f00fbf42
Ryan LaLiberty <rycolos@gkserver.net>
2023-08-14 07:23:15 -0400

    log to date_file in run.sh and log removal

--- infra/scripts/run.sh
+++ infra/scripts/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 cd /home/ubuntu/fitness-tracker-pipeline
+
 git pull >> /home/ubuntu/log/$(date +"%Y-%m-%d")_pull.log
+
 source dbt-venv/bin/activate
+
 cd dbt_fit
 dbt snapshot --profiles-dir=profiles >> /home/ubuntu/log/$(date +"%Y-%m-%d")_run.log
 dbt run --profiles-dir=profiles >> /home/ubuntu/log/$(date +"%Y-%m-%d")_run.log
